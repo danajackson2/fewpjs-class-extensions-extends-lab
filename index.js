3ffa78c046c021aaea96bb39bc6a56c9a4590913
danajackson2 <danajackson2@gmail.com>
2021-01-31 09:03:06 -0800

    Set object to find unique vales

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Your code here
+class Polygon{
+    constructor(arr){
+        this.sides = arr
+    }
+    get countSides(){
+        return this.sides.length
+    }
+    get perimeter(){
+        return this.sides.reduce((x, y) => x + y)
+    } 
+}
+
+class Triangle extends Polygon{
+    get isValid(){
+        return (this.sides[0] + this.sides[1] > this.sides[2]) && (this.sides[1] + this.sides[2] > this.sides[0]) && (this.sides[2] + this.sides[0] > this.sides[1])
+    }
+}
+
+class Square extends Polygon{
+    get isValid(){
+        return [...new Set(this.sides)].length === 1
+    }
+    get area(){
+        return this.sides[0]**2
+    }
+}
